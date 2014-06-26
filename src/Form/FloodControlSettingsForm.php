9cd051c1a6500f6c00acf3944c449142edcc7efd
AyramPapini <pinipapini@hotmail.com>
2014-06-26 14:51:37 -0300

    Update FloodControlSettingsForm.php

--- src/Form/FloodControlSettingsForm.php
+++ src/Form/FloodControlSettingsForm.php
@@ -11,24 +11,35 @@ class FloodControlSettingsForm extends ConfigFormBase {
     }
 
     public function buildForm(array $form, array &$form_state) {
-      $flood_config = $this->config('user.flood');
-
+      $flood_config = $this->configFactory->get('user.flood');
 
  $form['login'] = array(
     '#type' => 'fieldset',
     '#title' => t('Login'),
   );
   
-  $options = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 75, 100, 125, 150, 200, 250, 500
+  $options = array(
         '1' => '1',
-        '1' => '1',
-        '1' => '1',
-        '1' => '1',
-        '1' => '1',
-        '1' => '1',
-        '1' => '1',
-        '1' => '1',
-        
+        '2' => '2',
+        '3' => '3',
+        '4' => '4',
+        '5' => '5',
+        '6' => '6',
+        '7' => '7',
+        '8' => '8',
+        '9' => '9',
+        '10' => '10',
+        '20' => '20',
+        '30' => '30',
+        '40' => '40',
+        '50' => '50',
+        '75' => '75',
+        '100' => '100',
+        '125' => '125',
+        '150' => '150',
+        '200' => '200',
+        '250' => '250',
+        '500' => '500',
   );
   
   $form['login']['user_failed_login_ip_limit'] = array(
